5c6d9eae1f2bd93f88873e00ddb79c92e0303c6d
tsujitoe <tsujitoe@gmail.com>
2025-11-26 14:17:44 +0800

    ci: add django tests and github actions workflow

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomepageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        # 模擬一個使用者去瀏覽首頁 '/'
+        response = self.client.get('/')
+        # 斷言(Assert)：我們期望收到的狀態碼是 200 (代表成功)
+        self.assertEqual(response.status_code, 200)
